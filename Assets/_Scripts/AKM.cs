2136ec7045583ddb87ae9a9c3d707a136b0e0097
lajonss <gfs.b16@gmail.com>
2017-05-11 04:28:04 +0200

    first content

---
+++ Assets/_Scripts/AKM.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AKM : MonoBehaviour {
+    [SerializeField]
+    private AudioClip _ShotSound;
+
+    [SerializeField]
+    private float _Range;
+
+    private EllipsoidParticleEmitter _Sparks;
+	// Use this for initialization
+	void Start () {
+        _Sparks = gameObject.GetComponentInChildren<EllipsoidParticleEmitter>();
+        _Sparks.emit = false;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        Vector3 fwd = transform.TransformDirection(Vector3.forward);
+        RaycastHit hit;
+
+        if (Input.GetButtonDown("Fire1"))
+        {
+            _Sparks.Emit();
+            //audio.Play(); TODO
+
+            if (Physics.Raycast(transform.position, fwd, out hit))
+            {
+                if (hit.transform.tag == "Enemy" && hit.distance < _Range)
+                {
+                    Debug.Log("Trafiony przeciwnik");
+
+                }
+                else if (hit.distance < _Range)
+                {
+                    Debug.Log("Trafiona Sciana");
+                }
+            }
+        }
+    }
+}
---
+++ Assets/_Scripts/Bridge.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bridge : MonoBehaviour {
+    [SerializeField]
+    private float _InitialHealth = 100f;
+
+    [SerializeField]
+    private Texture2D _HealthTexture;
+
+    [SerializeField]
+    private int _HealthBarHeightPercentage = 10;
+
+    private float _CurrentHealth;
+    private float _HealthBarHeight;
+
+	void Start () {
+        _CurrentHealth = _InitialHealth;
+	}
+
+    private void Awake() {
+        _HealthBarHeight = 0.01f * _HealthBarHeightPercentage * Screen.height;
+    }
+
+    // Update is called once per frame
+    void Update () {
+		
+	}
+
+    private void OnGUI() {
+        GUI.DrawTexture(new Rect(0, 0, (_CurrentHealth / _InitialHealth) * Screen.width, _HealthBarHeight), _HealthTexture);
+    }
+}
